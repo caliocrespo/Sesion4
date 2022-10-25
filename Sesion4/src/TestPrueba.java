4e2730cf3b91df0036bfb9c3c1530c9a988e24b6
usuario <usuario@pc22-036>
2022-10-25 10:29:54 +0200

    terminando prueba

--- Sesion4/src/TestPrueba.java
+++ Sesion4/src/TestPrueba.java
@@ -1,17 +1,23 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestPrubea {
+class TestPrueba {
+	
+	@BeforeEach
+	void setUp() {
+		Cuenta.setSaldo(1000);
+	}
 
 	@Test
 	public void testIngreso() {
 		Cuenta.ingreso(1200);
-		assertEquals(1200,Cuenta.getSaldo());
+		assertEquals(2200,Cuenta.getSaldo());
 	}
 	
 	public void testReintegro() {
-		Cuenta.ingreso(1200);
+		Cuenta.ingreso(1000);
 		assertEquals(0,Cuenta.getSaldo());
 	}
 	
